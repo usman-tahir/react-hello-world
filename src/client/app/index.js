c0662de1de6c069f19b696751f6a26c8e3ee7c7f
usman-tahir <tahir.usman.ali94@outlook.com>
2017-10-06 15:41:49 -0400

    feat: updated the main App component to pull in the LikesComponent

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import LikesComponent from './LikesComponent';
+
 class App extends React.Component {
     render() {
         return (
-            <p>Hello React!</p>
+            <div>
+                <p>Hello React!</p>
+                <LikesComponent/>
+            </div>
         );
     }
 };
